5672475188685b787e52cd8550e9d415c807b0ff
Anthony Hopkins <ajpnhopk@uwaterloo.ca>
2019-03-24 07:39:47 -0400

    Added JS Server files to the repo

---
+++ JSServer/app/routes/stockit.routes.js
@@ -0,0 +1,9 @@
+module.exports = (app) => {
+	const stockits = require('../controllers/stockit.controller.js');
+	
+	app.post('/stockits', stockits.addaisle);
+	app.get('/stockits', stockits.retrieve);
+	app.get('/stockits/:aislenum', stockits.checkStatus);
+	app.post('/stockits/:aislenum', stockits.update);
+	app.delete('/stockits/:aislenum', stockits.delete);
+}
